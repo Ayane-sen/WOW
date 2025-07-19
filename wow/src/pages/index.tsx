4c51272cc26a757cf9f0ab62ed2ba367b4274245
mii <korondog777@gmail.com>
2025-07-19 18:38:10 +0900

    WOW

---
+++ wow/src/pages/index.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import styles from "./index.module.css";
+
+
+export default function Home() {
+  return (
+    <div className={styles.index}>
+      <main className={styles.main}>
+        <h1>WOW</h1>
+
+        {/*GIF画像を表示（優先度指定しない） */}
+        <img
+          src="/ハッカソンsmpl.gif"
+          alt="WOW GIF"
+          width={400}
+          height={300}
+        />
+
+        <p>これはWOWうさぎです。</p>
+      </main>
+    </div>
+  );
+}
