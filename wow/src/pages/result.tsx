c31ad2852f64dbe20f71df9931e16b40aa51daa4
mii <korondog777@gmail.com>
2025-07-25 23:07:44 +0900

    問題の出題の修正

--- wow/src/pages/result.tsx
+++ wow/src/pages/result.tsx
@@ -3,20 +3,20 @@ import {useRouter} from 'next/router';
 
 const ResultPage: React.FC=()=>{
     const router=useRouter();
-    const{ correct, totalCount } = router.query;
+    const{ correctCount, total } = router.query;
 
     //数値に変換
-    const correctCount=typeof correct === 'string' ? parseInt(correct, 10) : 0;
-    const totalquestions=typeof totalCount === 'string' ? parseInt(totalCount, 10) : 0;
+    const correctcount=typeof correctCount === 'string' ? parseInt(correctCount, 10) : 0;
+    const totalquestions=typeof total === 'string' ? parseInt(total, 10) : 0;
 
     //ホームページに戻る
     const handleBackToHome=()=>{
-        router.push('/index'); // ホームページにリダイレクト
+        router.push('/'); // ホームページにリダイレクト
     };
     return (
         <div>
             <h1>クイズ結果</h1>
-            <p>正解数: {correctCount} / {totalquestions}</p>
+            <p>正解数: {correctcount} / {totalquestions}</p>
             <button onClick={handleBackToHome}>ホームに戻る</button>
         </div>
     );
