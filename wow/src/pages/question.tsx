75da1e8b58d9c3ec4b8436120038048cc8ff1ec3
mii <korondog777@gmail.com>
2025-07-27 06:31:58 +0900

    questionの修正

--- wow/src/pages/question.tsx
+++ wow/src/pages/question.tsx
@@ -2,6 +2,7 @@ import React,{useState,useEffect} from 'react';
 import {useRouter} from 'next/router';
 import Button from '../components/Button/Button';
 import styles from '../styles/QuizPage.module.css';
+import { useSession, signIn } from 'next-auth/react';
 
 //クイズデータ型の定義
 interface QuizData {
