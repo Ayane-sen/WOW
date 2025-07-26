ce264dcfe6675be5fcb63e0654d0956a1109ab17
Ayane-sen <oto.momo1109@icloud.com>
2025-07-27 08:00:43 +0900

    クエストページ

--- wow/src/pages/quest_test.tsx
+++ wow/src/pages/quest_test.tsx
@@ -1,15 +1,25 @@
 import React from "react";
 import Button from '../components/Button/Button';
+import styles from '../styles/QuizPage.module.css';
 
 
 const TestPage: React.FC = () => {
     return(
-        <div>
-            <h1>テストページ</h1>
-            <Button
-            children
-            buttontype="quest"/>
+        <div className={styles.container}>
 
+            {/*上部大きいモニター */}
+            <div className={styles.topSection}>
+                問題
+            </div>
+
+            {/*中央4つの四角いモニター */}
+            <div className={styles.buttonGrid}>
+                <Button buttontype="quest">A</Button>
+                <Button buttontype="quest">B</Button>
+                <Button buttontype="quest">C</Button>
+                <Button buttontype="quest">D</Button>
+
+            </div>
         </div>
     );
 };
