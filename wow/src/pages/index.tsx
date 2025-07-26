024ca0a5e26b120e5a62df71c83d672974d65462
Urara0607 <muta.urara164@mail.kyutech.jp>
2025-07-26 14:59:50 +0900

    トップページ宇宙した

--- wow/src/pages/index.tsx
+++ wow/src/pages/index.tsx
@@ -15,22 +15,31 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>WOW</title>
       </Head>
-      <div className= {clsx(mobileStyles.background)}>
-        <div className={clsx(baseStyles.index, mobileStyles.index)}>
-        
-          <main className={clsx(baseStyles.main, mobileStyles.main)}>
-            
-            <div className={clsx(baseStyles.imageWrapper, mobileStyles.imageWrapper)}>
-              <img
-                src="/images/宇宙人ピンク.gif"
-                alt="宇宙のキャラクター"
-                className={clsx(baseStyles.character, mobileStyles.character)}
-              />
-            </div>
-            
-          </main>
-        </div>
+      <div className= {clsx(mobileStyles.backgroundSpace)}>
+        <div className= {clsx(mobileStyles.whiteStars)}></div>  
+        <div className= {clsx(mobileStyles.planet,mobileStyles.Earth)}></div> 
+        <div className= {clsx(mobileStyles.Sun)}></div> 
+        <div className= {clsx(mobileStyles.planet,mobileStyles.Jupiter)}></div> 
+        <div className= {clsx(mobileStyles.planet,mobileStyles.Mars)}></div> 
       </div>
+          <div className= {clsx(mobileStyles.backgroundShip)}>
+            <div className={clsx(baseStyles.index, mobileStyles.index)}>
+            
+              <main className={clsx(baseStyles.main, mobileStyles.main)}>
+                
+                <div className={clsx(baseStyles.imageWrapper, mobileStyles.imageWrapper)}>
+                  <img
+                    src="/images/宇宙人ピンク.gif"
+                    alt="宇宙のキャラクター"
+                    className={clsx(baseStyles.character, mobileStyles.character)}
+                  />
+                </div>
+                
+              </main>
+            </div>
+          </div>
+        
+      
     </>
   );
 }
